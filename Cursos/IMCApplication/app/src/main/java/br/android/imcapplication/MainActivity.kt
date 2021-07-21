68d39565906a4ed277355ac1643dafe6a6911cd6
IsaqueAraujoProfissional <isaquearaujo.profissional@gmail.com>
2021-07-20 21:03:10 -0300

    Constraint Latoyt, TextChanged, OnClickListerner e calcularIMC()

--- Cursos/IMCApplication/app/src/main/java/br/android/imcapplication/MainActivity.kt
+++ Cursos/IMCApplication/app/src/main/java/br/android/imcapplication/MainActivity.kt
@@ -2,10 +2,36 @@ package br.android.imcapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import androidx.core.widget.doAfterTextChanged
+import androidx.core.widget.doOnTextChanged
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        setListners()
+    }
+
+    private fun setListners(){
+        alturaEDT?.doAfterTextChanged { text ->
+            //Toast.makeText(this, text.toString(), Toast.LENGTH_SHORT).show()
+        }
+        pesoEDT?.doOnTextChanged{text, _, _, _ ->
+            //titleTXT.text = text
+        }
+        calcularBTN.setOnClickListener {
+            calcularIMC(pesoEDT.text.toString(), alturaEDT.text.toString())
+        }
+    }
+
+    private  fun calcularIMC(peso: String, altura: String){
+        val peso = peso.toFloatOrNull()
+        val altura = altura.toFloatOrNull()
+        if(peso != null && altura != null){
+            val imc = peso / (altura * altura)
+            titleTXT.text = "Seu IMC é: $imc".format(imc)
+        }
     }
 }
